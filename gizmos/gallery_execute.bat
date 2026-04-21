3a51401203f9d6123e7d28af531a769506e90f01
zx1360 <3039328600@qq.com>
2026-04-21 09:48:14 +0800

    无.

--- gizmos/gallery_execute.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running Gallery Execute Mode...
-.\gallery.exe -mode=execute
-pause
--- gizmos/gallery_ingest.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running Gallery Ingest Mode...
-.\gallery.exe -mode=ingest
-pause
